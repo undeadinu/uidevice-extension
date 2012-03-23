638464a7cf021f652c7ab705bb4ebdbca2c05dc9
Daniel <kokuyoku82@gmail.com>
2012-03-23 21:15:05 +0800

    Update data

--- UIDevice-Hardware.m
+++ UIDevice-Hardware.m
@@ -19,29 +19,33 @@
  
  iFPGA ->        ??
 
- iPhone1,1 ->    iPhone 1G, M68
+ iPhone1,1 ->    iPhone 1, M68
  iPhone1,2 ->    iPhone 3G, N82
  iPhone2,1 ->    iPhone 3GS, N88
- iPhone3,1 ->    iPhone 4/AT&T, N89
- iPhone3,2 ->    iPhone 4/Other Carrier?, ??
- iPhone3,3 ->    iPhone 4/Verizon, TBD
- iPhone4,1 ->    (iPhone 5/AT&T), TBD
- iPhone4,2 ->    (iPhone 5/Verizon), TBD
+ iPhone3,1 ->    iPhone 4/AT&T, GSM, N90
+ iPhone3,2 ->    iPhone 4, CDMA, Prototype
+ iPhone3,3 ->    iPhone 4/Verizon, CDMA, N92
+ iPhone4,1 ->    iPhone 4S/AT&T, N94
+ iPhone4,2 ->    (iPhone 4S/Verizon), TBD
 
- iPod1,1   ->    iPod touch 1G, N45
- iPod2,1   ->    iPod touch 2G, N72
- iPod2,2   ->    Unknown, ??
- iPod3,1   ->    iPod touch 3G, N18
- iPod4,1   ->    iPod touch 4G, N80
+ iPod1,1   ->    iPod touch 1, N45
+ iPod2,1   ->    iPod touch 2, N72
+ iPod2,2   ->    iPod touch 3, Prototype
+ iPod3,1   ->    iPod touch 3, N18
+ iPod4,1   ->    iPod touch 4, N81
  
  // Thanks NSForge
- iPad1,1   ->    iPad 1G, WiFi and 3G, K48
- iPad2,1   ->    iPad 2G, WiFi, K93
- iPad2,2   ->    iPad 2G, GSM 3G, K94
- iPad2,3   ->    iPad 2G, CDMA 3G, K95
- iPad3,1   ->    (iPad 3G, GSM)
- iPad3,2   ->    (iPad 3G, CDMA)
+ ipad0,1   ->    iPad, Prototype
+ iPad1,1   ->    iPad 1, WiFi and 3G, K48
+ iPad2,1   ->    iPad 2, WiFi, K93
+ iPad2,2   ->    iPad 2, GSM 3G, K94
+ iPad2,3   ->    iPad 2, CDMA 3G, K95
+ iPad2,4   ->    iPad 2, WiFi R2, K93A
+ iPad3,1   ->    The new iPad, WiFi
+ iPad3,2   ->    The new iPad, CDMA
+ iPad3,3   ->    The new iPad
 
+ iProd2,1   ->   AppleTV 2, Prototype
  AppleTV2,1 ->   AppleTV 2, K66
 
  i386, x86_64 -> iPhone Simulator
@@ -132,22 +136,23 @@
     if ([platform isEqualToString:@"iFPGA"])        return UIDeviceIFPGA;
 
     // iPhone
-    if ([platform isEqualToString:@"iPhone1,1"])    return UIDevice1GiPhone;
-    if ([platform isEqualToString:@"iPhone1,2"])    return UIDevice3GiPhone;
-    if ([platform hasPrefix:@"iPhone2"])            return UIDevice3GSiPhone;
-    if ([platform hasPrefix:@"iPhone3"])            return UIDevice4iPhone;
-    if ([platform hasPrefix:@"iPhone4"])            return UIDevice5iPhone;
+    if ([platform isEqualToString:@"iPhone1,1"])    return UIDeviceiPhone1;
+    if ([platform isEqualToString:@"iPhone1,2"])    return UIDeviceiPhone3G;
+    if ([platform hasPrefix:@"iPhone2"])            return UIDeviceiPhone3GS;
+    if ([platform hasPrefix:@"iPhone3"])            return UIDeviceiPhone4;
+    if ([platform hasPrefix:@"iPhone4"])            return UIDeviceiPhone4S;
     
     // iPod
-    if ([platform hasPrefix:@"iPod1"])             return UIDevice1GiPod;
-    if ([platform hasPrefix:@"iPod2"])              return UIDevice2GiPod;
-    if ([platform hasPrefix:@"iPod3"])              return UIDevice3GiPod;
-    if ([platform hasPrefix:@"iPod4"])              return UIDevice4GiPod;
+    if ([platform hasPrefix:@"iPod1"])             return UIDeviceiPod1;
+    if ([platform isEqualToString:@"iPod2,2"])      return UIDeviceiPod3;
+    if ([platform hasPrefix:@"iPod2"])              return UIDeviceiPod2;
+    if ([platform hasPrefix:@"iPod3"])              return UIDeviceiPod3;
+    if ([platform hasPrefix:@"iPod4"])              return UIDeviceiPod4;
 
     // iPad
-    if ([platform hasPrefix:@"iPad1"])              return UIDevice1GiPad;
-    if ([platform hasPrefix:@"iPad2"])              return UIDevice2GiPad;
-    if ([platform hasPrefix:@"iPad3"])              return UIDevice3GiPad;
+    if ([platform hasPrefix:@"iPad1"])              return UIDeviceiPad1;
+    if ([platform hasPrefix:@"iPad2"])              return UIDeviceiPad2;
+    if ([platform hasPrefix:@"iPad3"])              return UIDeviceTheNewiPad;
     
     // Apple TV
     if ([platform hasPrefix:@"AppleTV2"])           return UIDeviceAppleTV2;
@@ -170,26 +175,26 @@
 {
     switch ([self platformType])
     {
-        case UIDevice1GiPhone: return IPHONE_1G_NAMESTRING;
-        case UIDevice3GiPhone: return IPHONE_3G_NAMESTRING;
-        case UIDevice3GSiPhone: return IPHONE_3GS_NAMESTRING;
-        case UIDevice4iPhone: return IPHONE_4_NAMESTRING;
-        case UIDevice5iPhone: return IPHONE_5_NAMESTRING;
+        case UIDeviceiPhone1: return IPHONE_1_NAMESTRING;
+        case UIDeviceiPhone3G: return IPHONE_3G_NAMESTRING;
+        case UIDeviceiPhone3GS: return IPHONE_3GS_NAMESTRING;
+        case UIDeviceiPhone4: return IPHONE_4_NAMESTRING;
+        case UIDeviceiPhone4S: return IPHONE_4S_NAMESTRING;
         case UIDeviceUnknowniPhone: return IPHONE_UNKNOWN_NAMESTRING;
         
-        case UIDevice1GiPod: return IPOD_1G_NAMESTRING;
-        case UIDevice2GiPod: return IPOD_2G_NAMESTRING;
-        case UIDevice3GiPod: return IPOD_3G_NAMESTRING;
-        case UIDevice4GiPod: return IPOD_4G_NAMESTRING;
+        case UIDeviceiPod1: return IPOD_1_NAMESTRING;
+        case UIDeviceiPod2: return IPOD_2_NAMESTRING;
+        case UIDeviceiPod3: return IPOD_3_NAMESTRING;
+        case UIDeviceiPod4: return IPOD_4_NAMESTRING;
         case UIDeviceUnknowniPod: return IPOD_UNKNOWN_NAMESTRING;
             
-        case UIDevice1GiPad : return IPAD_1G_NAMESTRING;
-        case UIDevice2GiPad : return IPAD_2G_NAMESTRING;
-        case UIDevice3GiPad : return IPAD_3G_NAMESTRING;
+        case UIDeviceiPad1 : return IPAD_1_NAMESTRING;
+        case UIDeviceiPad2 : return IPAD_2_NAMESTRING;
+        case UIDeviceTheNewiPad : return THE_NEW_IPAD_NAMESTRING;
         case UIDeviceUnknowniPad : return IPAD_UNKNOWN_NAMESTRING;
             
-        case UIDeviceAppleTV2 : return APPLETV_2G_NAMESTRING;
-        case UIDeviceUnknownAppleTV: return APPLETV_UNKNOWN_NAMESTRING;
+        case UIDeviceAppleTV2 : return APPLE_TV_2_NAMESTRING;
+        case UIDeviceUnknownAppleTV: return APPLE_TV_UNKNOWN_NAMESTRING;
             
         case UIDeviceiPhoneSimulator: return IPHONE_SIMULATOR_NAMESTRING;
         case UIDeviceiPhoneSimulatoriPhone: return IPHONE_SIMULATOR_IPHONE_NAMESTRING;
