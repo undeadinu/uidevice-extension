7a12487be664415d78c2a6f0cad6e09c37c325fa
William Rust <wrust@kissmetrics.com>
2013-09-23 10:59:53 -0700

    Added iPhone 5C and 5S. Updated platforms comment. Cleaned up formatting

--- UIDevice-Hardware.h
+++ UIDevice-Hardware.h
@@ -8,23 +8,25 @@
 
 #define IFPGA_NAMESTRING                @"iFPGA"
 
-#define IPHONE_1_NAMESTRING            @"iPhone 1"
+#define IPHONE_1_NAMESTRING             @"iPhone 1"
 #define IPHONE_3G_NAMESTRING            @"iPhone 3G"
 #define IPHONE_3GS_NAMESTRING           @"iPhone 3GS" 
 #define IPHONE_4_NAMESTRING             @"iPhone 4" 
 #define IPHONE_4S_NAMESTRING            @"iPhone 4S"
 #define IPHONE_5_NAMESTRING             @"iPhone 5"
+#define IPHONE_5C_NAMESTRING            @"iPhone 5C"
+#define IPHONE_5S_NAMESTRING            @"iPhone 5S"
 #define IPHONE_UNKNOWN_NAMESTRING       @"Unknown iPhone"
 
-#define IPOD_1_NAMESTRING              @"iPod touch 1"
-#define IPOD_2_NAMESTRING              @"iPod touch 2"
-#define IPOD_3_NAMESTRING              @"iPod touch 3"
-#define IPOD_4_NAMESTRING              @"iPod touch 4"
-#define IPOD_5_NAMESTRING              @"iPod touch 5"
+#define IPOD_1_NAMESTRING               @"iPod touch 1"
+#define IPOD_2_NAMESTRING               @"iPod touch 2"
+#define IPOD_3_NAMESTRING               @"iPod touch 3"
+#define IPOD_4_NAMESTRING               @"iPod touch 4"
+#define IPOD_5_NAMESTRING               @"iPod touch 5"
 #define IPOD_UNKNOWN_NAMESTRING         @"Unknown iPod"
 
-#define IPAD_1_NAMESTRING              @"iPad 1"
-#define IPAD_2_NAMESTRING              @"iPad 2"
+#define IPAD_1_NAMESTRING               @"iPad 1"
+#define IPAD_2_NAMESTRING               @"iPad 2"
 #define THE_NEW_IPAD_NAMESTRING         @"The new iPad"
 #define IPAD_4G_NAMESTRING              @"iPad 4G"
 #define IPAD_MINI_NAMESTRING            @"iPad mini"
@@ -61,6 +63,10 @@ typedef enum {
     UIDeviceiPhone5,
     UIDeviceiPhone5GSM,
     UIDeviceiPhone5CDMA,
+    UIDeviceiPhone5CGSM,
+    UIDeviceiPhone5CGSMCDMA,
+    UIDeviceiPhone5SGSM,
+    UIDeviceiPhone5SGSMCDMA,
     
     UIDeviceiPod1,
     UIDeviceiPod2,
