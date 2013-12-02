ba6a8929a4d6b59d916c42a6f5e9b775e6d764ef
Ian G <yo.ian.g@gmail.com>
2013-12-02 01:20:35 -0500

    fix Example and have it test capabilities

--- Example/main.m
+++ Example/main.m
@@ -6,6 +6,7 @@
 
 #import <UIKit/UIKit.h>
 #import "UIDevice-Orientation.h"
+#import "UIDevice-Capabilities.h"
 
 #define COOKBOOK_PURPLE_COLOR [UIColor colorWithRed:0.20392f green:0.19607f blue:0.61176f alpha:1.0f]
 #define BARBUTTON(TITLE, SELECTOR) [[UIBarButtonItem alloc] initWithTitle:TITLE style:UIBarButtonItemStylePlain target:self action:SELECTOR]
@@ -32,6 +33,8 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(showOrientation:) userInfo:nil repeats:YES];
+    
+    [[UIDevice currentDevice] scanCapabilities];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
