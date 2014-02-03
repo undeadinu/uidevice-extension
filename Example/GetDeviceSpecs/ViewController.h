33ee36d13959748fff525b5b576179f0527bbfde
Ian Grossberg <ian@Andys-MacBook-Air.local>
2014-02-03 09:53:19 -0500

    show Hardware information

--- Example/GetDeviceSpecs/ViewController.h
+++ Example/GetDeviceSpecs/ViewController.h
@@ -12,4 +12,25 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, readwrite) IBOutlet UILabel *platform;
+@property (weak, readwrite) IBOutlet UILabel *hardwareModel;
+@property (weak, readwrite) IBOutlet UILabel *platformType;
+@property (weak, readwrite) IBOutlet UILabel *platformString;
+
+@property (weak, readwrite) IBOutlet UILabel *cpuFrequency;
+@property (weak, readwrite) IBOutlet UILabel *busFrequency;
+@property (weak, readwrite) IBOutlet UILabel *cpuCount;
+@property (weak, readwrite) IBOutlet UILabel *totalMemory;
+@property (weak, readwrite) IBOutlet UILabel *userMemory;
+
+@property (weak, readwrite) IBOutlet UILabel *totalDiskSpace;
+@property (weak, readwrite) IBOutlet UILabel *freeDiskSpace;
+
+@property (weak, readwrite) IBOutlet UILabel *macAddress;
+
+@property (weak, readwrite) IBOutlet UILabel *hasRetinaDisplay;
+@property (weak, readwrite) IBOutlet UILabel *has4InchDisplay;
+
+@property (weak, readwrite) IBOutlet UILabel *deviceFamily;
+
 @end
--- Example/GetDeviceSpecs/ViewController.m
+++ Example/GetDeviceSpecs/ViewController.m
@@ -10,14 +10,45 @@
 @interface ViewController ()
 @end
 
+@implementation NSString(EasyPeezy)
+
++ (NSString *)stringForUInt:(NSUInteger)value
+{
+    return [NSString stringWithFormat:@"%d", value];
+}
+
++ (NSString *)stringForBOOL:(BOOL)value
+{
+    NSString *result;
+    
+    if (value)
+    {
+        result = @"YES";
+    } else
+    {
+        result = @"NO";
+    }
+    
+    return result;
+}
+
+@end
+
 @implementation ViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	//TODO:include string in feedback email
-    
+
+    [self log];
+    [self updateUI];
+}
+
+- (void)log
+{
     UIDevice *currentDevice = [UIDevice currentDevice];
+    
     //NSString *model = [currentDevice model];
     NSString *systemVersion = [currentDevice systemVersion];
     NSString *serialnumber = [currentDevice serialnumber];
@@ -34,6 +65,32 @@
     
     NSString *deviceSpecs = [NSString stringWithFormat:@"%@ - %@ - %@ - %@ - %@ - %@ - %@ - %@", platformString, systemVersion, language, country, appVersion,serialnumber,modelnumber,RegionInfo];
     NSLog(@"Device Specs --> %@",deviceSpecs);
+}
+
+- (void)updateUI
+{
+    UIDevice *currentDevice = [UIDevice currentDevice];
+    
+    self.platform.text = [currentDevice platform];
+    self.hardwareModel.text = [currentDevice hwmodel];
+    self.platformType.text = [NSString stringForUInt:[currentDevice platformType] ];
+    self.platformString.text = [currentDevice platformString];
+
+    self.cpuFrequency.text = [NSString stringForUInt:[currentDevice cpuFrequency] ];
+    self.busFrequency.text = [NSString stringForUInt:[currentDevice busFrequency] ];
+    self.cpuCount.text = [NSString stringForUInt:[currentDevice cpuCount] ];
+    self.totalMemory.text = [NSString stringForUInt:[currentDevice totalMemory] ];
+    self.userMemory.text = [NSString stringForUInt:[currentDevice userMemory] ];
+    
+    self.totalDiskSpace.text = [NSString stringForUInt:[currentDevice totalDiskSpace] ];
+    self.freeDiskSpace.text = [NSString stringForUInt:[currentDevice freeDiskSpace] ];
+    
+    self.macAddress.text = [currentDevice macaddress];
+    
+    self.hasRetinaDisplay.text = [NSString stringForBOOL:[UIDevice hasRetinaDisplay] ];
+    self.has4InchDisplay.text = [NSString stringForBOOL:[UIDevice has4InchDisplay] ];
+    
+    self.deviceFamily.text = [NSString stringForUInt:[currentDevice deviceFamily] ];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
